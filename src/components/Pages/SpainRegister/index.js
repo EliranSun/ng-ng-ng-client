ba5722dd8dd449b0eacf7c0be2b1187fd743bfb4
Eliran Shemesh <eliran@steps.me>
2022-04-04 17:21:17 +0300

    Post template register cleanups

---
+++ src/components/Pages/SpainRegister/index.js
@@ -0,0 +1,1 @@
+export { default as SpainRegister } from "./SpainRegister";
--- src/components/Templates/Register/Register.jsx
+++ src/components/Templates/Register/Register.jsx
@@ -1,48 +1,48 @@
-import { Stepper, StepperContext } from "../../Molecules";
+import { Stepper } from "../../Molecules";
 import {
-  Birthdate,
-  Currency,
-  Email,
-  FirstName,
-  LastName,
-  MiddleName,
-  Password,
-  PhoneNumber,
-  RepeatPassword,
-  Username,
+	Birthdate,
+	Currency,
+	Email,
+	FirstName,
+	LastName,
+	MiddleName,
+	Password,
+	PhoneNumber,
+	RepeatPassword,
+	Username,
 } from "../../Molecules/Fields";
 
 const CreateAccount = () => {
-  return (
-    <div>
-      <Username />
-      <FirstName />
-      <MiddleName />
-      <LastName />
-      <Birthdate />
-    </div>
-  );
+	return (
+		<div>
+			<Username />
+			<FirstName />
+			<MiddleName />
+			<LastName />
+			<Birthdate />
+		</div>
+	);
 };
 
 const RegisterDetails = () => {
-  return (
-    <div>
-      <Email />
-      <PhoneNumber />
-      <Password />
-      <RepeatPassword />
-      <Currency />
-    </div>
-  );
+	return (
+		<div>
+			<Email />
+			<PhoneNumber />
+			<Password />
+			<RepeatPassword />
+			<Currency />
+		</div>
+	);
 };
 
 const Register = () => {
-  return (
-    <Stepper withProgress>
-      <CreateAccount />
-      <RegisterDetails />
-    </Stepper>
-  );
+	return (
+		<Stepper withProgress>
+			<CreateAccount />
+			<RegisterDetails />
+		</Stepper>
+	);
 };
 
 export default Register;
